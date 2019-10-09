d605f097f7dd53b12622f34cfabedf9d5a294f9e
noalTessman <noaltessman@gmail.com>
2019-10-08 19:48:33 -0600

    Added scatter plot grapher with specific data

--- projects/Earfquake/earthquakegrapher.py
+++ projects/Earfquake/earthquakegrapher.py
@@ -2,6 +2,6 @@ import plotly.express as pf
 import pandas as pd
 def graphData():
     df = pd.read_csv("./quakes.csv")
-    fig = pf.line(df, x = "Quake Number",y = "Magnitude", title = "Earthquakes above 4 Magnitude after September")
+    fig = pf.scatter(df, x = "Quake Number",y = "Magnitude", title = "Earthquakes above 4 Magnitude after September",hover_data=['Location'])
     fig.show()
 graphData()
